4937a3c9a5afc870f798ab92a1d29957b8bd7f87
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-18 04:04:38 +0200

    Ultimate test for Display

--- main.cpp
+++ main.cpp
@@ -9,15 +9,24 @@ int main()
 {
     BinaryTree<int> tree;
 
-    tree.Add(50);
-    tree.Add(30);
     tree.Add(20);
+    tree.Add(10);
+    tree.Add(5);
+    tree.Add(1);
+    tree.Add(6);
+    tree.Add(7);
+    tree.Add(15);
+    tree.Add(13);
+    tree.Add(12);
+    tree.Add(14);
+    tree.Add(17);
+    tree.Add(18);
+    tree.Add(25);
+    tree.Add(21);
+    tree.Add(27);
+    tree.Add(30);
     tree.Add(40);
-    tree.Add(80);
-    tree.Add(60);
-    tree.Add(120);
-    tree.Add(90);
-    tree.Add(100);
+    tree.Add(50);
     //tree.Add(10);
     //tree.Add(30);
 
